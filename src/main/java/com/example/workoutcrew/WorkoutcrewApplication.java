547533afdcefc37da09b55131a20de3a950bdae5
Cheongwon Seo <cjddnjs96@ajou.ac.kr>
2026-08-23 17:06:44 +0900

    init: init

---
+++ src/main/java/com/example/workoutcrew/WorkoutcrewApplication.java
@@ -0,0 +1,13 @@
+package com.example.workoutcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutcrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutcrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workoutcrew/WorkoutcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
